4c2cbf6299cb3b4bccfd03e148914096ad6584ca
Shaik mahammad jani <68095466+skjani314@users.noreply.github.com>
2024-12-14 05:01:25 +0000

    cors issue

--- Server/index.js
+++ Server/index.js
@@ -40,7 +40,7 @@ app.use(cookieParser());
 
 app.use(cors({
   origin: (origin, callback) => {
-    const allowedOrigins = ['https://mess-monetering.vercel.app','http://localhost:3000'];
+    const allowedOrigins = ['https://mess-monetering.vercel.app','http://localhost:5173'];
     if (origin && (allowedOrigins.includes(origin) || origin.endsWith('.vercel.app'))) {
         callback(null, true); 
     } else {
