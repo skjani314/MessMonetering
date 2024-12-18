8244a07ddf92a055a84e9f69acf6bdcf5ab12b3f
ganesh-basavoju <basavojuganesh@gmail.com>
2024-12-18 22:55:54 +0530

    student page B changed

--- Client/mess/src/pages/Student/Student.jsx
+++ Client/mess/src/pages/Student/Student.jsx
@@ -18,7 +18,7 @@ import React, { useEffect, useState } from 'react';
 import { useContext } from "react"
 import { Spin, Row, Col, Flex, Typography } from "antd";
 import Header from '../../components/Header/Header';
-import StudentSidebar from '../../components/sidebar/StudentSideBar';
+import StudentSidebar from '../../components/sidebar/StudentSidebar';
 import MenuTable from '../../components/Home/MenuTable';
 import Context from '../../context/Context';
 import { FaPills, FaSquareFull } from 'react-icons/fa';
