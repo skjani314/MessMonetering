15be9b2882932ebef0c1d3ede21cffe23eb77e21
Shaik Mahammad jani <skskjani7@gmail.com>
2024-12-20 23:43:42 +0530

    fornt not linked to backend properly

--- Server/models/Dtokens.js
+++ Server/models/Dtokens.js
@@ -3,7 +3,7 @@ import mongoose, { mongo } from "mongoose";
 const DtokenSchema=new mongoose.Schema({
 
 user_id:{type:mongoose.Schema.Types.ObjectId,ref:'users',required:true},
-token:{required:true,type:String}
+token:{required:true,type:String,unique:true}
 
 })
 
