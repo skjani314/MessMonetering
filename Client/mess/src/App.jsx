3ed00f8b8f7396679dcbb8d5de10cdc896dda4cc
Shaik Mahammad jani <skskjani7@gmail.com>
2024-12-21 18:01:45 +0530

    success ah

--- Client/mess/src/App.jsx
+++ Client/mess/src/App.jsx
@@ -46,7 +46,6 @@ function App() {
 
   window.receiveData = (data) => {
     localStorage.setItem("userToken", data);
-    success(data)
   };
   const changeActiveTab = (tabId) => {
     setActiveTab(tabId);
